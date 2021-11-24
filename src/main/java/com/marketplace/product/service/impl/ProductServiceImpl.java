0fe5e169ac075b4763ca965f728112685c63b3e2
LeandroPillaCampos <leannpilla@hotmail.com>
2021-11-24 17:07:49 -0300

    fix port conection

--- src/main/java/com/marketplace/product/service/impl/ProductServiceImpl.java
+++ src/main/java/com/marketplace/product/service/impl/ProductServiceImpl.java
@@ -8,7 +8,6 @@ import com.marketplace.product.controller.request.ReserveProductRequest;
 import com.marketplace.product.domain.mapper.PutProductSkuMapper;
 import com.marketplace.product.domain.model.Product;
 import com.marketplace.product.exception.InventoryNotNegativeException;
-import com.marketplace.product.repositories.KeywordRepository;
 import com.marketplace.product.repositories.ProductRepository;
 import com.marketplace.product.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
